d73ddc07dfa9b9e94ce2251f0140ee1adaff4805
bradley sevison <bradleysev@gmail.com>
2017-08-05 18:24:01 -0600

    added some logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,48 +1,41 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC0oWtk_ydkOoZgr3E7gINtt9v_NBKX_xY",
-    authDomain: "trainscheduler-15e8c.firebaseapp.com",
-    databaseURL: "https://trainscheduler-15e8c.firebaseio.com",
-    projectId: "trainscheduler-15e8c",
-    storageBucket: "",
-    messagingSenderId: "203035204501"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyC0oWtk_ydkOoZgr3E7gINtt9v_NBKX_xY",
+  authDomain: "trainscheduler-15e8c.firebaseapp.com",
+  databaseURL: "https://trainscheduler-15e8c.firebaseio.com",
+  projectId: "trainscheduler-15e8c",
+  storageBucket: "",
+  messagingSenderId: "203035204501"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  database.ref().on('value', function(snapshot){
-    console.log(snapshot.val());
-    var snapshotVal = snapshot.val();
-    for (var i = 0; i < snapshotVal.length; i++){
-        var row = $('<tr>')
-        row.append('<td>'+ snapshotVal[i].name + '</td>' + '<td>'+ snapshotVal[i].destination + '</td>' + '<td>'+ snapshotVal[i].frequency + '</td>' +
-            '<td>'+ snapshotVal[i].firstTrainTime + '</td>' + '<td>'+ snapshotVal[i].nextArrival + '</td>' + '<td>'+ snapshotVal[i].minutesAway + '</td>');
-        
-        ('#trainTable').append(row);
-    }
-  });
+database.ref().on('value', function (snapshot) {
+  console.log(snapshot.val());
+  
+});
 
-  $('document').ready(function(){
+$('document').ready(function () {
 
-    $('#submitBtn').on('click', function(){
-        console.log('hello')
-        var name = $('#trainNameInput').val().trim();
-        var dest = $('#destinationInput').val().trim();
-        var freq = $('#frequencyInput').val().trim();
-        var first = moment($('#firstTrainTimeInput').val().trim()).format('HH:mm a');
-        var next;
-        var away;
-        database.ref().push({
-            name: name,
-            destination: dest,
-            frequency: freq,
-            firstTrainTime: first,
-            nextArrival: null,
-            minutesAway: null,
-        });
-        $('#trainTable').append('<tr>' + '<td>'+ name + '</td>' + '<td>'+ dest + '</td>' + '<td>'+ freq + '</td>' +
-            '<td>'+ first + '</td>' + '<td>'+ next + '</td>' + '<td>'+ away + '</td>' + '</tr>');
+  $('#submitBtn').on('click', function () {
+    console.log('hello')
+    var name = $('#trainNameInput').val().trim();
+    var dest = $('#destinationInput').val().trim();
+    var freq = $('#frequencyInput').val().trim();
+    var first = $('#firstTrainTimeInput').val().trim();
+    var next = moment(first, 'HH:mm').add(freq, 'minute').calendar();
+    var away;
+    database.ref().push({
+      name: name,
+      destination: dest,
+      frequency: freq,
+      firstTrainTime: moment(first, 'HH:mm').format('h:mm a'),
+      nextArrival: next,
+      minutesAway: null,
     });
-
+    $('#trainTable').append('<tr>' + '<td>' + name + '</td>' + '<td>' + dest + '</td>' + '<td>' + freq + '</td>' + '<td>' +
+      next + '</td>' + '<td>' + away + '</td>' + '</tr>');
   });
+
+});
